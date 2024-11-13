c83364bc3583e4b982e9e330926d42a995ef85c8
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-11-13 12:14:58 -0500

    added menu items

--- src/Components/Layout/Nav.jsx
+++ src/Components/Layout/Nav.jsx
@@ -6,25 +6,28 @@ function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light position-fixed w-100">
             <div className="container-fluid">
-                <NavLink to="/" className="logoNav navbar-brand py-3 px-2 ms-auto">
+                <NavLink to="/" className="logoNav navbar-brand py-3 px-2 me-auto">
                     <img src={logo} className="w-100" alt="Izo Decor Glass Logo" />
                 </NavLink>
-                {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="mainNav collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav mx-auto">
-                        <li className="nav-item">
-                            <NavLink to="/" className="nav-link active text-uppercase fs-5" aria-current="page">Home</NavLink>
+                        <li className="navLiItem nav-item">
+                            <NavLink to="/" className="navLinkItem nav-link active text-uppercase fs-5" aria-current="page">Izo Decor Glass</NavLink>
                         </li>
-                        <li className="nav-item">
-                            <NavLink to={"/"} className="nav-link text-uppercase fs-5" href="#">Features</NavLink>
+                        <li className="navLiItem nav-item">
+                            <NavLink to={"/"} className="navLinkItem nav-link text-uppercase fs-5 ms-lg-4" href="#">Usluge</NavLink>
                         </li>
-                        <li className="nav-item">
-                            <NavLink to={"/"} className="nav-link text-uppercase fs-5" href="#">Pricing</NavLink>
+                        <li className="navLiItem nav-item">
+                            <NavLink to={"/"} className="navLinkItem nav-link text-uppercase fs-5 ms-lg-4" href="#">Tipovi Stakla</NavLink>
+                        </li>
+                        <li className="navLiItem nav-item">
+                            <NavLink to={"/"} className="navLinkItem nav-link text-uppercase fs-5 ms-lg-4" href="#">Kontakt</NavLink>
                         </li>
                     </ul>
-                </div> */}
+                </div>
             </div>
         </nav>
     )
