dc3e5abe6d88f1c22ce5c879aa3d6ae5193e328f
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-11-14 17:36:44 -0500

    ....

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,28 +1,54 @@
 import HeaderLogo from "../../assets/Images/Logo/IZO-DECOR-GLASS-Logo-Large .webp";
-import { Link } from "react-router-dom";
+import { MdOutlinePriceChange } from "react-icons/md";
+import { VscTools } from "react-icons/vsc";
+import { FaRegSmile } from "react-icons/fa";
+// import { Link } from "react-router-dom";
 
 
 function Header() {
   return (
-    <header className="headerHome vh-100 row w-100">
-      <div className="col-2">
+    <header className="headerHome vh-100 row">
+      <div className="col-lg-2">
         <img src={HeaderLogo} className="logoHeader" alt="Izo Decor Glass Logo" />
       </div>
-      <div className="headerUsluge col-10 d-flex justify-content-end align-items-end">
-        <div className="headerUslugeContainer me-2">
-          <p className="text-center mt-2 fs-5 fontW700">Usluge 1</p>
-          <p className="px-3 fontW600">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
-          <Link to={"/"} className="headerLink text-decoration-none text-white fontW700 px-3">Saznaj Vise &nbsp; &#10132;</Link>
+      <div className="headerUsluge col-lg-10 d-flex flex-column flex-md-row justify-content-end align-items-end p-0 mb-5">
+        <div className="headerUslugeContainer me-md-2 d-flex flex-column justify-content-center align-items-center">
+          <div>
+              <FaRegSmile  className="headerIcon mt-2"/>
+          </div>
+          <p className="px-3 mt-3 text-center fontW600">Zadovoljstvo svakog klijenta je naš prioritet</p>
+
+          {/* <div>
+            <p className="text-center mt-2 mb-1 fs-5 fontW700">Usluge 1</p>
+            <p className="px-3 fontW600">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+          </div>
+          <Link to={"/"} className="headerLink text-decoration-none text-white fontW700 px-3">Saznaj Vise &nbsp; &#10132;</Link> */}
         </div>
-        <div className="headerUslugeContainer me-2">
-           <p className="text-center mt-2 fs-5 fontW700">Usluge 2</p>
-           <p className="px-3 fontW600">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
-          <Link to={"/"} className="headerLink text-decoration-none text-white fontW700 px-3">Saznaj Vise &nbsp; &#10132;</Link>
+        <div className="headerUslugeContainer me-md-2 d-flex flex-column justify-content-center align-items-center">
+          <div>
+              <VscTools  className="headerIcon mt-2"/>
+          </div>
+          <p className="px-3 mt-3 text-center fontW600">Široka ponuda staklorezačkih usluga prilagođenih vašim potrebama.</p>
+
+          {/* <div>
+            <p className="text-center mt-2 fs-5 fontW700">Usluge 2</p>
+            <p className="px-3 fontW600">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+          </div>
+          <Link to={"/"} className="headerLink text-decoration-none text-white fontW700 px-3">Saznaj Vise &nbsp; &#10132;</Link> */}
         </div>
-        <div className="headerUslugeContainer">
-           <p className="text-center mt-2 fs-5 fontW700">Usluge 3</p>
-           <p className="px-3 fontW600">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
-          <Link to={"/"} className="headerLink text-decoration-none text-white fontW700 px-3">Saznaj Vise &nbsp; &#10132;</Link>
+        <div className="headerUslugeContainer me-md-2 d-flex flex-column justify-content-center align-items-center">
+          <div>
+              <MdOutlinePriceChange className="headerIcon mt-2"/>
+          </div>
+          <p className="px-3 mt-3 text-center fontW600">Nudimo konkurentne cene bez kompromisa na kvalitetu.</p>
+
+
+
+          {/* <div>
+            <p className="text-center mt-2 fs-5 fontW700">Usluge 3</p>
+            <p className="px-3 fontW600">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+          </div>
+          <Link to={"/"} className="headerLink text-decoration-none text-white fontW700 px-3">Saznaj Vise &nbsp; &#10132;</Link> */}
         </div>  
            
       </div>
