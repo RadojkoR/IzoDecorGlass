ec3480c8f46d967039b630d71efa63d9c3c900e0
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-11-14 12:14:09 -0500

    ....

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -10,19 +10,19 @@ function Header() {
       </div>
       <div className="headerUsluge col-10 d-flex justify-content-end align-items-end">
         <div className="headerUslugeContainer me-2">
-          <p className="text-center mt-2 text-light fs-5 fontW700">Usluge 1</p>
-          <p className="px-3 text-">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
-          <Link to={"/"} className="headerLink text-decoration-none text-white fontW600 px-3">Saznaj Vise &nbsp; &#10132;</Link>
+          <p className="text-center mt-2 fs-5 fontW700">Usluge 1</p>
+          <p className="px-3 fontW600">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+          <Link to={"/"} className="headerLink text-decoration-none text-white fontW700 px-3">Saznaj Vise &nbsp; &#10132;</Link>
         </div>
         <div className="headerUslugeContainer me-2">
-           <p className="text-center mt-2 text-light fs-5 fontW700">Usluge 2</p>
-           <p className="px-3 text-">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
-          <Link to={"/"} className="headerLink text-decoration-none text-white fontW600 px-3">Saznaj Vise &nbsp; &#10132;</Link>
+           <p className="text-center mt-2 fs-5 fontW700">Usluge 2</p>
+           <p className="px-3 fontW600">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+          <Link to={"/"} className="headerLink text-decoration-none text-white fontW700 px-3">Saznaj Vise &nbsp; &#10132;</Link>
         </div>
         <div className="headerUslugeContainer">
-           <p className="text-center mt-2 text-light fs-5 fontW700">Usluge 3</p>
-           <p className="px-3 text-">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
-          <Link to={"/"} className="headerLink text-decoration-none text-white fontW600 px-3">Saznaj Vise &nbsp; &#10132;</Link>
+           <p className="text-center mt-2 fs-5 fontW700">Usluge 3</p>
+           <p className="px-3 fontW600">Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+          <Link to={"/"} className="headerLink text-decoration-none text-white fontW700 px-3">Saznaj Vise &nbsp; &#10132;</Link>
         </div>  
            
       </div>
