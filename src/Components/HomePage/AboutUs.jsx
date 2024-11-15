47fd7fced65910ae91a882d87c8aaf832f8ebb1b
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-11-15 11:11:45 -0500

    ...

--- src/Components/HomePage/AboutUs.jsx
+++ src/Components/HomePage/AboutUs.jsx
@@ -7,11 +7,11 @@ function AboutUs() {
       <div className="paralaxColor">
         <div className="aboutUsContainer container py-5 row m-auto">
           <h2 className="text-center mb-5 fs-1 izoText fontW700 pb-5">O Nama</h2>
-          <article className="col-lg-7 col-12">
-            <h3 className="oNamaHeader mb-4">Izo Decor Glass</h3>
-            <p className="fs-5 px-3">Dobrodošli u <span className="izoText fontW700">IZO Decor Glass</span>, vašu novu destinaciju za visokokvalitetnu obradu i dizajn stakla. Iako smo mlada firma, naša strast prema staklorezačkom zanatu i posvećenost savršenstvu nas motivišu da se stalno usavršavamo i pratimo najnovije tehnologije u industriji. Sa željom da vam pružimo najbolje rešenje za svaki projekat, bilo da je u pitanju staklo za enterijer ili eksterijer, trudimo se da svakom klijentu ponudimo jedinstvenu uslugu prilagođenu njegovim potrebama.</p>
-            <p className="fs-5 px-3">Naša vizija je da kroz kvalitetne proizvode, preciznu izradu i profesionalan pristup postanemo vaš prvi izbor za sve vrste staklorezačkih usluga. Vaša podrška i zadovoljstvo su nam prioritet, a naš cilj je da doprinesemo lepoti i funkcionalnosti vašeg prostora, unoseći jedinstvene elemente stakla koje će trajati godinama.</p>
-            <p className="fs-5 px-3">Hvala što ste se upoznali sa <span className="izoText fontW600">IZO Decor Glass</span>. Radujemo se što ćemo imati priliku da sarađujemo i stvaramo vrhunske staklene elemente za vaš prostor.</p>
+          <article className="col-lg-7 col-12 p-0">
+            <h3 className="oNamaHeader mb-4 px-md-3">Izo Decor Glass</h3>
+            <p className="fs-5 px-md-3">Dobrodošli u <span className="izoText fontW700">IZO Decor Glass</span>, vašu novu destinaciju za visokokvalitetnu obradu i dizajn stakla. Iako smo mlada firma, naša strast prema staklorezačkom zanatu i posvećenost savršenstvu nas motivišu da se stalno usavršavamo i pratimo najnovije tehnologije u industriji. Sa željom da vam pružimo najbolje rešenje za svaki projekat, bilo da je u pitanju staklo za enterijer ili eksterijer, trudimo se da svakom klijentu ponudimo jedinstvenu uslugu prilagođenu njegovim potrebama.</p>
+            <p className="fs-5 px-md-3">Naša vizija je da kroz kvalitetne proizvode, preciznu izradu i profesionalan pristup postanemo vaš prvi izbor za sve vrste staklorezačkih usluga. Vaša podrška i zadovoljstvo su nam prioritet, a naš cilj je da doprinesemo lepoti i funkcionalnosti vašeg prostora, unoseći jedinstvene elemente stakla koje će trajati godinama.</p>
+            <p className="fs-5 px-md-3">Hvala što ste se upoznali sa <span className="izoText fontW600">IZO Decor Glass</span>. Radujemo se što ćemo imati priliku da sarađujemo i stvaramo vrhunske staklene elemente za vaš prostor.</p>
             <div className="d-flex flex-column ">
               <p className="fs-5 px-3 mb-0 fontW700 izoText">Boško Miletić</p>
               <p className="fs-6 fontW600 px-3 ">Osnivač i vlasnik, Izo Decor Glass</p>
