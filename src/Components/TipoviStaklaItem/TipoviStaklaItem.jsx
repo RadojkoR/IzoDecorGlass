c6e5532216e8af6d1c040993cbec5ee6f70d1ed8
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-11-20 13:30:34 -0500

    ....

--- src/Components/TipoviStaklaItem/TipoviStaklaItem.jsx
+++ src/Components/TipoviStaklaItem/TipoviStaklaItem.jsx
@@ -1,15 +1,31 @@
+import PropTypes from "prop-types";
 
 
-
-function tipoviStaklaItem({title, description}) {
+function TipoviStaklaItem({title, description, img, paralaxClass}) {
   return (
-    <section className='tipoviStaklaContainer py-5 container-md my-5 border-2 border-bottom'>
+    <>
+      <section className={`paralax ${paralaxClass}`}></section>
+    <section className='tipoviStaklaContainer pb-4 container-md my-5'>
         <h2>{title}</h2>
-        <article className="tipoviStakla">
-          <p className="fs-5 px-md-3">{description}</p>
+        <article className="tipoviStakla row mt-5">
+          <div className="col-md-8">
+            <p className="fs-5 px-md-3">{description}</p>
+          </div>
+          <div className="tipStaklaImg col-md-4">
+            <img src={img} className="w-100" alt="" />
+          </div>
         </article>
       </section>
+    </>
+    
   )
 }
 
-export default tipoviStaklaItem
+TipoviStaklaItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  paralaxClass: PropTypes.string.isRequired,
+};
+
+export default TipoviStaklaItem
--- src/Pages/TipoviStakla.jsx
+++ src/Pages/TipoviStakla.jsx
@@ -1,5 +1,9 @@
 import { SecondaryHeader } from "../Components/Layout"
 import TipoviStaklaItem from "../Components/TipoviStaklaItem/TipoviStaklaItem"
+import clearGlassImg from "../assets/Images/clearGlass.webp";
+import temperedGlassImg from "../assets/Images/temperedGlass.webp";
+import frostedGlassImg from "../assets/Images/frostedGlass.webp";
+import laminatedGlassImg from "../assets/Images/laminatedGlass.webp";
 
 
 function TipoviStakla() {
@@ -10,19 +14,27 @@ function TipoviStakla() {
   const typeOfGlass = [
     {
       title: "Providno Staklo",
-      description: "Klasičan i univerzalan izbor koji se uklapa u svaki prostor. Providno staklo omogućava maksimalnu prolaznost svetlosti, što ga čini idealnim za prozore, tuš kabine, staklene pregrade"
+      description: "Klasičan i univerzalan izbor koji se uklapa u svaki prostor. Providno staklo omogućava maksimalnu prolaznost svetlosti, što ga čini idealnim za prozore, tuš kabine, staklene pregrade",
+      img: clearGlassImg,
+      paralaxClass: "paralaxOne"
     },
     {
       title: "Kaljeno staklo",
-      description: "Kaljeno staklo je poznato po svojoj čvrstoći i otpornosti na udarce. U slučaju lomljenja, razbija se na sitne komade bez oštrih ivica, čineći ga izuzetno sigurnim. Pogodno je za Balkonske i stepenišne ograde, Staklene stolove,Staklene stolove"
+      description: "Kaljeno staklo je poznato po svojoj čvrstoći i otpornosti na udarce. U slučaju lomljenja, razbija se na sitne komade bez oštrih ivica, čineći ga izuzetno sigurnim. Pogodno je za Balkonske i stepenišne ograde, Staklene stolove,Staklene stolove",
+      img: temperedGlassImg,
+      paralaxClass: "paralaxTwo"
     },
     {
       title: "Matirano staklo",
-      description: "Odlično za postizanje privatnosti bez gubitka prirodnog osvetljenja. Njegova blago zamućena površina idealna je za Kupatilske prozore, Tuš kabine, Kancelarijske pregrade"
+      description: "Odlično za postizanje privatnosti bez gubitka prirodnog osvetljenja. Njegova blago zamućena površina idealna je za Kupatilske prozore, Tuš kabine, Kancelarijske pregrade",
+      img: frostedGlassImg,
+      paralaxClass: "paralaxThree"
     },
     {
       title: "Laminirano staklo",
-      description: "Laminirano staklo se sastoji od dva ili više slojeva stakla povezanih folijom koja sprečava njegovo raspadanje pri lomljenju. Ovo ga čini savršenim za Zaštitu od provala, Izolaciju buke, Spoljna stakla na višespratnicama"
+      description: "Laminirano staklo se sastoji od dva ili više slojeva stakla povezanih folijom koja sprečava njegovo raspadanje pri lomljenju. Ovo ga čini savršenim za Zaštitu od provala, Izolaciju buke, Spoljna stakla na višespratnicama",
+      img: laminatedGlassImg,
+      paralaxClass: "paralaxFour"
     }
   ]
 
@@ -31,14 +43,13 @@ function TipoviStakla() {
       <SecondaryHeader bgHeader="tipoviStaklaHeader" headerInfo={tipoviStaklaHeaderTxt}/>
       <section className='tipoviStaklaContainer py-5 container-md my-4 '>
         <h2>Tipovi Stakla &#8213; Savršeno rešenje za svaki prostor</h2>
-        <article className="tipoviStakla">
+        <article className="tipoviStakla mt-4">
           <p className="fs-5 px-md-3">Staklo je materijal koji kombinuje estetiku, funkcionalnost i trajnost. Bez obzira na to da li opremate poslovni prostor, dom ili eksterijer, pravilan odabir tipa stakla ključan je za postizanje željenog izgleda i funkcije. Predstavljamo vam najčešće vrste stakla koje možete koristiti:</p>
         </article>
       </section>
-      <section className="tipoviStaklaParalax"></section>
       {
         typeOfGlass.map((type,index) => (
-          <TipoviStaklaItem key={index} title={type.title} description={type.description}/>
+          <TipoviStaklaItem key={index} title={type.title} description={type.description} img={type.img} paralaxClass={type.paralaxClass}/>
         ))
       }
      
