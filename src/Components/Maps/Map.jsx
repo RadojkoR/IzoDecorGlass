8e2f1d3b46fa41426297ba8e60d521e56feab1d3
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-11-22 00:02:08 -0500

    finished kontakt page

---
+++ src/Components/Maps/Map.jsx
@@ -0,0 +1,19 @@
+
+
+function Map() {
+  return (
+    <section className="container-md mx-auto mb-5">
+        <h3 className="mb-5 text-center">Čačak</h3>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55051.54069167731!2d20.320243258979175!3d43.88940190676117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4757723fdaf79e2d%3A0x46460a390c51b8ce!2zxIxhxI1haywgU2VyYmlh!5e1!3m2!1sen!2sca!4v1732249655372!5m2!1sen!2sca" 
+        width="100%"
+        height="300" 
+        style={{border:0}}
+        allowFullScreen="" 
+        loading="lazy" 
+        referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+    </section>
+  )
+}
+
+export default Map
--- src/Pages/Kontakt.jsx
+++ src/Pages/Kontakt.jsx
@@ -1,11 +1,77 @@
-
-
-
+import { SecondaryHeader } from "../Components/Layout";
+import Map from "../Components/Maps/Map";
+import { PiMapPinAreaDuotone } from "react-icons/pi";
+import { LuPhoneOutgoing } from "react-icons/lu";
+import { MdOutlineEmail } from "react-icons/md";
+import logo from "../assets/Images/Logo/IZO-DECOR-GLASS-Logo-Small.webp"
 
 function Kontakt() {
+  const kontaktHeaderTxt = [
+    {
+      headingEl: "Kontakt",
+      para: "Staklena rešenja na dohvat ruke - kontaktirajte nas!",
+    },
+  ];
   return (
-    <div>Kontakt</div>
-  )
+    <>
+      <SecondaryHeader bgHeader="kontaktHeader" headerInfo={kontaktHeaderTxt} />
+      <section className="container-md my-5">
+        <h2 className="mt-5 mb-4">Kontaktirajte nas</h2>
+        <p className="fs-5 px-md-3">
+          Uvek smo spremni za nove projekte.Ako ste zainteresovani za naša
+          staklena rešenja, slobodno nas kontaktirajte putem jednog od sledećih
+          kanala, uključujući naš{" "}
+          <span className="text-primary">Izo Decor Glass Chat</span>{" "}
+          <span className="">za brzu komunikaciju</span> za brzu komunikaciju.
+        </p>
+      </section>
+      <section className="kontaktContainer d-flex flex-column border-bottom container-md mx-auto pb-5 mb-5">
+        <div className="row">
+          <article className="addressContainerKontakt col-lg-4 col-sm-12 col-xs-12 d-flex mb-4">
+            <div className="kontaktIconContainer me-4 d-flex justify-content-center align-items-center">
+              <PiMapPinAreaDuotone className="w-50 h-50" />
+            </div>
+            <div className="kontaktContentContainer">
+              <h4 className="mb-3">Adresa</h4>
+              <a className="text-decoration-none" href="#">
+                Adresa ide ovde, Čačak
+              </a>
+            </div>
+          </article>
+          <article className="phoneContainerKontakt col-lg-4 col-sm-12 col-xs-12 d-flex mb-4">
+            <div className="kontaktIconContainer me-4 d-flex justify-content-center align-items-center">
+              <LuPhoneOutgoing className=" w-50 h-50" />
+            </div>
+            <div className="kontaktContentContainer">
+              <h4 className="mb-3">Telefon</h4>
+              <a className="text-decoration-none" href="tel:123456789">
+                123456789
+              </a>
+            </div>
+          </article>
+          <article className="phoneContainerKontakt col-lg-4 col-sm-12 col-xs-12 d-flex mb-4">
+            <div className="kontaktIconContainer me-4 d-flex justify-content-center align-items-center">
+              <MdOutlineEmail className="w-50 h-50" />
+            </div>
+            <div className="kontaktContentContainer">
+              <h4 className="mb-3">Email</h4>
+              <a className="text-decoration-none" href="mailto:123456789">
+                email@test.com
+              </a>
+            </div>
+          </article>
+        </div>
+        <div className="d-flex flex-column mx-auto mt-5">
+          <p className="fs-5 px-3 mb-0 fontW700 izoText">Boško Miletić</p>
+          <p className="fs-6 fontW600 px-3 ">
+            Osnivač i vlasnik, Izo Decor Glass
+          </p>
+        </div>
+        <img src={logo} className="kontaktLogo ms-auto" alt="Izo Decor Glass Logo" />
+      </section>
+      <Map />
+    </>
+  );
 }
 
-export default Kontakt
+export default Kontakt;
