9238a876714b56c899377c435acb060abe255172
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-11-25 18:35:59 -0500

    ....

--- src/Components/HomePage/AboutUs.jsx
+++ src/Components/HomePage/AboutUs.jsx
@@ -12,7 +12,7 @@ function AboutUs() {
       <div className="paralaxColor">
         <div className="aboutUsContainer container-md py-5 row m-auto">
           <h2 className="text-center mb-5 fs-1 izoText fontW700 pb-5" data-aos="fade-down">O Nama</h2>
-          <article className="col-lg-7 col-12 p-0" data-aos="fade-right">
+          <article className="col-lg-7 col-12 p-0" data-aos="flip-right">
             <h3 className="oNamaHeader mb-4 px-md-3">Izo Decor Glass</h3>
             <p className="fs-5 px-md-3">Dobrodošli u <span className="izoText fontW700">IZO Decor Glass</span>, vašu novu destinaciju za visokokvalitetnu obradu i dizajn stakla. Iako smo mlada firma, naša strast prema staklorezačkom zanatu i posvećenost savršenstvu nas motivišu da se stalno usavršavamo i pratimo najnovije tehnologije u industriji. Sa željom da vam pružimo najbolje rešenje za svaki projekat, bilo da je u pitanju staklo za enterijer ili eksterijer, trudimo se da svakom klijentu ponudimo jedinstvenu uslugu prilagođenu njegovim potrebama.</p>
             <p className="fs-5 px-md-3">Naša vizija je da kroz kvalitetne proizvode, preciznu izradu i profesionalan pristup postanemo vaš prvi izbor za sve vrste staklorezačkih usluga. Vaša podrška i zadovoljstvo su nam prioritet, a naš cilj je da doprinesemo lepoti i funkcionalnosti vašeg prostora, unoseći jedinstvene elemente stakla koje će trajati godinama.</p>
@@ -23,7 +23,7 @@ function AboutUs() {
             </div>
             
           </article>
-          <article className="aboutUsImgContainer col-lg-5 col-12" data-aos="fade-left">
+          <article className="aboutUsImgContainer col-lg-5 col-12" data-aos="flip-right">
 
           </article>
 
