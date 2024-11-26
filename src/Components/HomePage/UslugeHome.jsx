5ddf3c4263a3d68829557d55d9986b41d7995639
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-11-25 23:57:51 -0500

    ....

--- src/Components/HomePage/UslugeHome.jsx
+++ src/Components/HomePage/UslugeHome.jsx
@@ -12,7 +12,7 @@ function UslugeHome() {
                     data-aos-delay="100">
                         {/* <GiDrill className="fs-1 mt-4 mb-3 text-danger" /> */}
                         <h3 className="fs-4 fontW600 izoText">Obrada Stakla</h3>
-                        <p className="text-center text-black-50 mt-3">Izrađujemo staklo po meri uz precizno bušenje, brušenje i fazetiranje. Pružamo visok kvalitet i savršen završni izgled za svaki prostor.</p>
+                        <p className="text-center textLight mt-3">Izrađujemo staklo po meri uz precizno bušenje, brušenje i fazetiranje. Pružamo visok kvalitet i savršen završni izgled za svaki prostor.</p>
                         <Link to={"#"} className="btn ms-auto saznajViseBtn fontW600 mt-4 mt-auto" >Saznaj Vise</Link>
                     </article>
                     <article 
@@ -21,7 +21,7 @@ function UslugeHome() {
                     data-aos-delay="300">
                         {/* <LiaPencilRulerSolid className="fs-1 mt-4 mb-3 text-danger" /> */}
                         <h3 className="fs-4 fontW600 izoText">Tuš Kabine</h3>
-                        <p className="text-center text-black-50 mt-3">Izrađujemo staklene tuš kabine po meri, spajajući funkcionalnost i moderan dizajn. Kvalitetna obrada i lako održavanje za vaš idealan prostor.</p>
+                        <p className="text-center textLight mt-3">Izrađujemo staklene tuš kabine po meri, spajajući funkcionalnost i moderan dizajn. Kvalitetna obrada i lako održavanje za vaš idealan prostor.</p>
                         <Link to={`/usluge/tušKabine`} className="btn ms-auto saznajViseBtn fontW600 mt-auto" >Saznaj Vise</Link>
                     </article>
                     <article 
@@ -30,7 +30,7 @@ function UslugeHome() {
                     data-aos-delay="500">
                         {/* <BsTools className="fs-1 mt-4 mb-3 text-danger" /> */}
                         <h3 className="fs-4 fontW600 izoText">Staklene Ograde</h3>
-                        <p className="text-center text-black-50 mt-3">Izrađujemo staklene ograde koje kombinuju sigurnost i estetski dizajn. Idealno rešenje za vaš enterijer i eksterijer.</p>
+                        <p className="text-center textLight mt-3">Izrađujemo staklene ograde koje kombinuju sigurnost i estetski dizajn. Idealno rešenje za vaš enterijer i eksterijer.</p>
                         <Link to={`/usluge/stakleneOgrade`} className="btn ms-auto saznajViseBtn fontW600 mt-auto" >Saznaj Vise</Link>
                     </article>
                     <article 
@@ -39,7 +39,7 @@ function UslugeHome() {
                     data-aos-delay="700">
                         {/* <GiLaurelsTrophy className="fs-1 mt-4 mb-3 text-danger" /> */}
                         <h3 className="fs-4 fontW600 izoText">Ogledala</h3>
-                        <p className="text-center text-black-50 mt-3">Nudimo ogledala po meri, savršen spoj funkcionalnosti i estetike za vaš prostor. Kvalitetna izrada i elegantan dizajn.</p>
+                        <p className="text-center textLight mt-3">Nudimo ogledala po meri, savršen spoj funkcionalnosti i estetike za vaš prostor. Kvalitetna izrada i elegantan dizajn.</p>
                         <Link to={`/usluge/ogledala`} className="btn ms-auto saznajViseBtn fontW600 mt-auto" >Saznaj Vise</Link>
                     </article>
                     <article 
@@ -48,7 +48,7 @@ function UslugeHome() {
                     data-aos-delay="100">
                         {/* <GiLaurelsTrophy className="fs-1 mt-4 mb-3 text-danger" /> */}
                         <h3 className="fs-4 fontW600 izoText">Stakleni Zidovi</h3>
-                        <p className="text-center text-black-50 mt-3">Stakleni zidovi pružaju modernu i elegantnu podelu prostora. Idealno rešenje za prozračnost i svetlost u vašem enterijeru.</p>
+                        <p className="text-center textLight mt-3">Stakleni zidovi pružaju modernu i elegantnu podelu prostora. Idealno rešenje za prozračnost i svetlost u vašem enterijeru.</p>
                         <Link to={`/usluge/stakleniZidovi`} className="btn ms-auto saznajViseBtn fontW600 mt-auto" >Saznaj Vise</Link>
                     </article>
                     <article 
@@ -57,7 +57,7 @@ function UslugeHome() {
                     data-aos-delay="300">
                         {/* <GiLaurelsTrophy className="fs-1 mt-4 mb-3 text-danger" /> */}
                         <h3 className="fs-4 fontW600 izoText">Staklena Vrata</h3>
-                        <p className="text-center text-black-50 mt-3">Izrađujemo staklena vrata po meri koja spajaju eleganciju i funkcionalnost. Savršeno za moderan i svetao prostor.</p>
+                        <p className="text-center textLight mt-3">Izrađujemo staklena vrata po meri koja spajaju eleganciju i funkcionalnost. Savršeno za moderan i svetao prostor.</p>
                         <Link to={`/usluge/staklenaVrata`} className="btn ms-auto saznajViseBtn fontW600 mt-auto" >Saznaj Vise</Link>
                     </article>
                     <article 
@@ -66,7 +66,7 @@ function UslugeHome() {
                     data-aos-delay="500">
                         {/* <GiLaurelsTrophy className="fs-1 mt-4 mb-3 text-danger" /> */}
                         <h3 className="fs-4 fontW600 izoText">Zidne Obloge od Stakla</h3>
-                        <p className="text-center text-black-50 mt-3">Elegantne i funkcionalne staklene obloge za zidove koje unose modernost i lakoću održavanja u vaš prostor.</p>
+                        <p className="text-center textLight mt-3">Elegantne i funkcionalne staklene obloge za zidove koje unose modernost i lakoću održavanja u vaš prostor.</p>
                         <Link to={`/usluge/stakleneObloge`} className="btn ms-auto saznajViseBtn fontW600 mt-auto" >Saznaj Vise</Link>
                     </article>
                 </div>
