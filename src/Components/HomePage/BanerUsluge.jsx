b25d55ca4e1334bebbe23df8e1d77480bde5dfa1
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-11-27 00:00:28 -0500

    ....

--- src/Components/HomePage/BanerUsluge.jsx
+++ src/Components/HomePage/BanerUsluge.jsx
@@ -21,7 +21,7 @@ function BanerUsluge() {
                         data-aos-delay="100">
                         <GiDrill className="fs-1 mt-4 mb-3 text-danger" />
                         <h2 className="fs-4 fontW600 izoText">Obrada Stakla</h2>
-                        <p className="text-center text-black-50 mt-4">Precizno bušenje, brušenje, fazetiranje i još mnogo toga</p>
+                        <p className="text-center textLight mt-4">Precizno bušenje, brušenje, fazetiranje i još mnogo toga</p>
                     </article>
                     <article
                         className="banerItemContainer col-lg-3 col-6 d-flex flex-column align-items-center my-2"
@@ -29,7 +29,7 @@ function BanerUsluge() {
                         data-aos-delay="300">
                         <LiaPencilRulerSolid className="fs-1 mt-4 mb-3 text-danger" />
                         <h2 className="fs-4 fontW600 izoText">Staklo po vašoj meri</h2>
-                        <p className="text-center text-black-50 mt-4">Ogledala, tuš kabine, stakleni zidovi i ograde prilagođeni vašim potrebama i prostoru.</p>
+                        <p className="text-center textLight mt-4">Ogledala, tuš kabine, stakleni zidovi i ograde prilagođeni vašim potrebama i prostoru.</p>
                     </article>
                     <article
                         className="banerItemContainer col-lg-3 col-6 d-flex flex-column align-items-center my-2"
@@ -37,7 +37,7 @@ function BanerUsluge() {
                         data-aos-delay="500">
                         <BsTools className="fs-1 mt-4 mb-3 text-danger" />
                         <h2 className="fs-4 fontW600 izoText">Stručna montaža</h2>
-                        <p className="text-center text-black-50 mt-4">Ogledala, tuš kabine, stakleni zidovi i ograde prilagođeni vašim potrebama i prostoru.</p>
+                        <p className="text-center textLight mt-4">Ogledala, tuš kabine, stakleni zidovi i ograde prilagođeni vašim potrebama i prostoru.</p>
                     </article>
                     <article
                         className="banerItemContainer col-lg-3 col-6 d-flex flex-column align-items-center my-2"
@@ -45,7 +45,7 @@ function BanerUsluge() {
                         data-aos-delay="700">
                         <GiLaurelsTrophy className="fs-1 mt-4 mb-3 text-danger" />
                         <h2 className="fs-4 fontW600 izoText">Visok kvalitet</h2>
-                        <p className="text-center text-black-50 mt-4">Ogledala, tuš kabine, stakleni zidovi i ograde prilagođeni vašim potrebama i prostoru.</p>
+                        <p className="text-center textLight mt-4">Ogledala, tuš kabine, stakleni zidovi i ograde prilagođeni vašim potrebama i prostoru.</p>
                     </article>
                 </div>
             </section>
