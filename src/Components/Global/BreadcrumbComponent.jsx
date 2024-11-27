928c19b6a13efb74c6e20bbea797388d16c1793b
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-11-26 23:23:40 -0500

    ...

--- src/Components/Global/BreadcrumbComponent.jsx
+++ src/Components/Global/BreadcrumbComponent.jsx
@@ -3,27 +3,8 @@ import { Link, useLocation } from "react-router-dom";
 function BreadcrumbComponent() {
     const location = useLocation();
     const pathParts = location.pathname.split("/").filter(Boolean);
-    console.log((location.pathname));
-    
-    
-        
-    // Funkcija za formatiranje putanje
-    // const formatPath = (path) => {
-    //     // Dekodiranje URL enkodiranih karaktera, kao što je %C5%A1 za š
-    //     path = decodeURIComponent(path);
-
-    //     // Dodavanje razmaka pre svakog velikog slova, bez obzira na prethodno slovo
-    //     // Ova funkcija dodaje razmak pre svakog velikog slova
-    //     path = path.replace(/([A-Z])/g, ' $&');
-
-    //     // Pretvaranje prvog slova u veliko
-    //     path = path.charAt(0).toUpperCase() + path.slice(1);
-
-    //     return path;
-    // };
-    
   return (
-    <section className="routePageContainer d-flex align-items-center">
+    <section className="breadcrumbContainer d-flex align-items-center">
         <p className="m-0 ms-5">
             <Link to={"/"} className="routePageLink">Home</Link>
            { pathParts.map((item, index) => (
