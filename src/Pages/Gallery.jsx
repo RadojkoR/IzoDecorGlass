80a2ef386f4d8f7dcc3252c19ce3d6c3346c8ef7
RadojkoRadulovic <radulovicradojko@gmail.com>
2025-05-10 12:46:20 -0400

    added lightbox in gallery

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import { SecondaryHeader } from "../Components/Layout";
 import { ColumnsPhotoAlbum } from "react-photo-album";
-import galleryData from "../Data/imageGalleryData";
 import "react-photo-album/columns.css";
+import galleryData from "../Data/imageGalleryData";
+import Lightbox from "yet-another-react-lightbox";
+import { Counter, Fullscreen, Thumbnails, Zoom, Download, Captions } from "yet-another-react-lightbox/plugins";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/counter.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+
+
+
 
 function Gallery() {
+  const [open, setOpen] = useState(false)
+  const [index, setIndex] = useState(-1)
+
   const galerijaHeaderTxt = [
     {
       headingEl: "Galerija",
@@ -29,10 +42,24 @@ function Gallery() {
           <ColumnsPhotoAlbum
             photos={galleryData}
             columns={(containerWidth) => {
-              if(containerWidth < 576) return 3;
-              if(containerWidth < 768) return 4;
+              if (containerWidth < 576) return 3;
+              if (containerWidth < 768) return 4;
               return 5;
             }}
+            onClick={({ index }) => {
+              setIndex(index)
+              setOpen(true)
+            }}
+          />
+          <Lightbox
+            slides={galleryData}
+            open={open}
+            close={() => setOpen(false)}
+            index={index}
+            plugins={[Counter, Fullscreen, Thumbnails, Zoom, Download, Captions]}
+            counter={{ container: { style: { top: "unset", bottom: 0, right: 0, left: "unset" } } }}
+            title={{container:{style: { top: "unset", bottom: 0, right: 0, left: "unset" }}}}
+
           />
         </article>
 
