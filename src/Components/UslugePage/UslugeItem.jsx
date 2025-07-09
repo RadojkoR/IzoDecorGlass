36189e425d95377cac43ca926bf0f87860e749c1
RadojkoRadulovic <radulovicradojko@gmail.com>
2025-07-08 23:21:12 -0400

    fixed url on usluge item component

--- src/Components/UslugePage/UslugeItem.jsx
+++ src/Components/UslugePage/UslugeItem.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
 
-function UslugeItem({ heading, para, img, paralaxClass, showBtn }) {
-  const linkPath = heading
-    .replace(/\s+/g, '')
-    .replace(/^./, str => str.toLowerCase());
+function UslugeItem({ heading, para, img, paralaxClass, showBtn, url }) {
+  // const linkPath = heading
+  //   .replace(/\s+/g, '')
+  //   .replace(/^./, str => str.toLowerCase());
   return (
     <>
       <section className='uslugeContainer pb-4 container-md p-2 mb-5'>
@@ -12,7 +12,7 @@ function UslugeItem({ heading, para, img, paralaxClass, showBtn }) {
         <article className="uslugeContentContainer row g-0 mt-5">
           <div className="col-lg-8 col-12 d-flex flex-column">
             <p className="fs-5 px-md-3">{para}</p>
-            <Link to={`/usluge/${linkPath}`} className={`btn ms-auto saznajViseBtn fontW600 mt-4 ${showBtn}`}>Saznaj Više &nbsp;<span className='fs-5'>&#10154;</span></Link>
+            <Link to={url} className={`btn ms-auto saznajViseBtn fontW600 mt-4 ${showBtn}`}>Saznaj Više &nbsp;<span className='fs-5'>&#10154;</span></Link>
           </div>
           <div className="uslugeImgContainer col-lg-4 col-12 mx-auto">
             <img src={img} className="w-100" alt="" />
@@ -30,6 +30,7 @@ UslugeItem.propTypes = {
   img: PropTypes.string.isRequired,
   paralaxClass: PropTypes.string.isRequired,
   showBtn: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
 };
 
 export default UslugeItem
